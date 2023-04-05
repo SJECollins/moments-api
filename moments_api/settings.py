1502b6a35ee1a689e4147d5603815af8af86c1c9
SCollins <sje.collins@gmail.com>
2023-04-05 09:04:12 +0000

    Typo!

--- moments_api/settings.py
+++ moments_api/settings.py
@@ -23,7 +23,7 @@ CLOUDINARY_STORAGE = {
 
 MEDIA_URL = '/media/'
 
-DEFAULT_FILE_SOTRAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
